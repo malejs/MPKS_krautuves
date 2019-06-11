997c8b6b6e7d1ea8eb3e9caf396738f94c3e399d
malejs <34074178+malejs@users.noreply.github.com>
2019-06-11 19:15:26 +0300

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -907,7 +907,7 @@ $(window).on('load', function() {
       credit += ' | ';
     }
 
-    credit += 'View <a href="' + getSetting('_githubRepo') + '">code</a>';
+    credit += 'Apskatīt <a href="' + getSetting('_githubRepo') + '">code</a>';
     if (getSetting('_codeCredit')) credit += ' by ' + getSetting('_codeCredit');
     credit += ' with ';
     $('.leaflet-control-attribution')[0].innerHTML = credit + attributionHTML;
