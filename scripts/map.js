ff3233980085937c24e8b02c59ef75c090048e69
malejs <34074178+malejs@users.noreply.github.com>
2019-06-11 19:23:18 +0300

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -907,8 +907,8 @@ $(window).on('load', function() {
       credit += ' | ';
     }
 
-    credit += 'Apskatīt <a href="' + getSetting('_githubRepo') + '">code</a>';
-    if (getSetting('_codeCredit')) credit += ' by ' + getSetting('_codeCredit');
+    credit += 'Apskatīt <a href="' + getSetting('_githubRepo') + '">lapu</a>';
+    if (getSetting('_codeCredit')) credit += ' no ' + getSetting('_codeCredit');
     credit += ' with ';
     $('.leaflet-control-attribution')[0].innerHTML = credit + attributionHTML;
   }
